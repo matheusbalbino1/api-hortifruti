0b105aba872679de14a808b696d7718ddd3620be
Matheus <matheusbalbinodeoliveira3@gmail.com>
2022-08-07 14:04:18 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
-import {Request, response, Response} from "express";
+import { Request, response, Response } from "express";
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 
-interface dbProps{
-    genus:string;
-    name:string;
-    id:number;
-    family:string;
-    order:string;
-    nutritions:{
-        carbohydrates:number;
-        protein:number;
+interface dbProps {
+    genus: string;
+    name: string;
+    id: number;
+    family: string;
+    order: string;
+    nutritions: {
+        carbohydrates: number;
+        protein: number;
         fat: number;
-        calories:number;
+        calories: number;
         sugar: number;
     };
 };
 
 
 app.use(express.json())
-app.use((req:Request,res:Response, next:any)=>{
-    res.header("Acess-Control-Allow-Origin","");
+app.use((req: Request, res: Response, next: any) => {
+    res.header("Acess-Control-Allow-Origin", "");
     app.use(cors());
     next();
 })
 
 const delay = 0;
-const db:dbProps[] = require("./db.json");
+const db: dbProps[] = require("./db.json");
 
 app.get("/api/fruit/all", (req: Request, res: Response) => {
     res.setHeader("Access-Control-Allow-Origin", "https://matheusbalbino1.github.io/eccomerce-hortifruti/")
-    setTimeout(()=>{
+    setTimeout(() => {
         return res.json(db);
     }, delay);
-  
+
 });
 
 app.get("/api/fruit/:id", (req: Request, res: Response) => {
-
+    res.setHeader("Access-Control-Allow-Origin", "https://matheusbalbino1.github.io/")
     const params = req.params.id;
 
-    const fruit = db.filter((fruta)=>{ return fruta.id == Number(params) })
+    const fruit = db.filter((fruta) => { return fruta.id == Number(params) })
 
-    setTimeout(()=>{
+    setTimeout(() => {
         return res.status(200).send(JSON.stringify(fruit))
     }, delay);
-  
+
 });
 
 
